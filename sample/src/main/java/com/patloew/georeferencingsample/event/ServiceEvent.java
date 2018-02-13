f22ebd728119b80f5d2d91a2b1c4857cffc2a88f
maciek252 <maciej.szreter@gmail.com>
2018-02-13 11:20:38 +0100

    service

---
+++ sample/src/main/java/com/patloew/georeferencingsample/event/ServiceEvent.java
@@ -0,0 +1,7 @@
+package com.patloew.georeferencingsample.event;
+
+/**
+ * Created by gunhansancar on 06/04/16.
+ */
+public class ServiceEvent {
+}
---
+++ sample/src/main/java/com/patloew/georeferencingsample/service/MyService.java
@@ -0,0 +1,87 @@
+package com.patloew.georeferencingsample.service;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+
+
+import com.patloew.georeferencingsample.event.ServiceEvent;
+import com.patloew.georeferencingsample.event.SimpleEvent;
+
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by gunhansancar on 06/04/16.
+ */
+public class MyService extends Service {
+
+    private Timer timer;
+    private AtomicInteger counter = new AtomicInteger();
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        startTimer();
+        return START_STICKY_COMPATIBILITY;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        EventBus.getDefault().register(this);
+
+        startTimer();
+
+    }
+
+    @Override
+    public void onDestroy() {
+        EventBus.getDefault().unregister(this);
+        stopTimer();
+        super.onDestroy();
+    }
+
+    @Subscribe
+    public void onEvent(ServiceEvent event) {
+        stopTimer();
+        stopSelf();
+    }
+
+    private void startTimer() {
+        stopTimer();
+        timer = new Timer();
+        timer.scheduleAtFixedRate(new SimpleTimerTask(counter), 0, 3000);
+    }
+
+    private void stopTimer() {
+        if (timer != null) {
+            timer.cancel();
+            timer = null;
+        }
+    }
+
+    private static class SimpleTimerTask extends TimerTask {
+
+        private AtomicInteger current;
+
+        public SimpleTimerTask(AtomicInteger current) {
+            this.current = current;
+        }
+
+        @Override
+        public void run() {
+            EventBus.getDefault().postSticky(new SimpleEvent(current.addAndGet(1)));
+        }
+    }
+}
